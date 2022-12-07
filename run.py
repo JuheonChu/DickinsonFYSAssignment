c6770f3e2c15421c3b0d120457eba5ffd018dfa9
Juheon Chu <35699839+JuheonChu@users.noreply.github.com>
2022-12-06 23:19:41 -0500

    Update run.py

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ citizenship_df = pd.read_excel('Dickinson First Year Seminar.xlsx', sheet_name =
 gender_df = pd.read_excel('Dickinson First Year Seminar.xlsx', sheet_name = 'gender')
 obj_coef_df = pd.read_excel('Dickinson First Year Seminar.xlsx', sheet_name = 'obj_coef')
 rank_df = pd.read_excel('Dickinson First Year Seminar.xlsx', sheet_name = 'rank_weights')
-
+course_df = pd.read_excel('Dickinson First Year Seminar.xlsx', sheet_name = 'course_num')
 
 
 
@@ -33,10 +33,12 @@ rank_df = pd.read_excel('Dickinson First Year Seminar.xlsx', sheet_name = 'rank_
 STUDENTS = gender_df['stu_id'].tolist()
 
 
-SEMINARS = [1,2,3, 4,5,6,9,10,11,
-		        12,13,14,15,16,17,18,19,20,21,22,
-		        23,24,25,26,28,29,30,31,32,33,34,35,36,37,
-		        39,41,42,44,45,46,47]
+SEMINARS = course_df['seminar_no'].tolist()
+
+#[1,2,3, 4,5,6,9,10,11,
+#		        12,13,14,15,16,17,18,19,20,21,22,
+#		        23,24,25,26,28,29,30,31,32,33,34,35,36,37,
+#		        39,41,42,44,45,46,47]
 
 # initializing seminar pick lists from each student
 SEMINAR_PICK = student_choices_df['rank'].tolist()
