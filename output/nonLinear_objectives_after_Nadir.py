67365b40b756513a2297456f1fe6e9befdbc7c80
Juheon Chu <35699839+JuheonChu@users.noreply.github.com>
2023-02-19 23:45:53 -0500

    Update nonLinear_objectives_after_Nadir.py

--- output/nonLinear_objectives_after_Nadir.py
+++ output/nonLinear_objectives_after_Nadir.py
@@ -31,10 +31,6 @@ STUDENTS = gender_df['stu_id'].tolist()
 
 SEMINARS = course_df['seminar_no'].tolist()
 
-#[1,2,3, 4,5,6,9,10,11,
-#		        12,13,14,15,16,17,18,19,20,21,22,
-#		        23,24,25,26,28,29,30,31,32,33,34,35,36,37,
-#		        39,41,42,44,45,46,47]
 
 # initializing seminar pick lists from each student
 SEMINAR_PICK = student_choices_df['rank'].tolist()
