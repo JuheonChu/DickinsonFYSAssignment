42064446dcb5dcf627fdab6e83f33dac999becfd
Juheon Chu <35699839+JuheonChu@users.noreply.github.com>
2023-03-28 10:04:23 -0400

    Update linearized_objectives.py

--- output/linearized_objectives.py
+++ output/linearized_objectives.py
@@ -215,7 +215,7 @@ for k in SEMINARS:
     model.addConstr(w_gender[k] >= MSEM[k] - FSEM[k])
     model.addConstr(w_gender[k] >= FSEM[k] - MSEM[k])
 
-for j in SEMINARS:
+for k in SEMINARS:
     model.addConstr(w_citizenship[k] >= MSEM[k] - FSEM[k])
     model.addConstr(w_citizenship[k] >= FSEM[k] - MSEM[k])
     
