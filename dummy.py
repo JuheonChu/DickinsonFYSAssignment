e7a1590f173030bedb7e7f75f602649fcbd3c6d1
Juheon Chu <35699839+JuheonChu@users.noreply.github.com>
2023-04-01 01:55:11 -0400

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,11 +1,7 @@
-# Set the constraints for each gender and student-type objectives
-# e.g. w_gender[1] = |MSEM[1] - FSEM[1]|
-for k in SEMINARS:
-    model.addConstr(w_gender[k] >= MSEM[k] - FSEM[k])
-    model.addConstr(w_gender[k] >= FSEM[k] - MSEM[k])
-for k in SEMINARS:
-    model.addConstr(w_citizenship[k] >= US_SEM[k] - NonUS_SEM[k])
-    model.addConstr(w_citizenship[k] >= NonUS_SEM[k] - US_SEM[k])
+# Ensure to allow relative tolerance for less-prioritized objectives
+zU_Rank = model.getObjective().getValue()
+model.addConstr(f_rank <= 0.999999999 * zU_Rank, "Rank")
+model.addConstr(g <= 1.0000000001 * zU_Gender, "Gender")
 
-model.setObjective(sum(w_gender).values(), GRB.MINIMIZE)
+model.setObjective(g, GRB.MINIMIZE)
 model.optimize()
