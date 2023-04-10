caceda325950f5d28ffb3b3652d117e90a5b1648
Juheon Chu <35699839+JuheonChu@users.noreply.github.com>
2023-04-10 10:51:49 -0400

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,7 +1,13 @@
-# Ensure to allow relative tolerance for less-prioritized objectives
-zU_Rank = model.getObjective().getValue()
-model.addConstr(f_rank <= 0.999999999 * zU_Rank, "Rank")
-model.addConstr(g <= 1.0000000001 * zU_Gender, "Gender")
-
-model.setObjective(g, GRB.MINIMIZE)
-model.optimize()
+# Add constraints for Gender Penalty and Citizenship Penalty
+for k in MSEM:
+  # Minimize the Gender Penalty
+  model.addConstr(gen_penalty[k] >= MSEM[k] - FSEM[k])
+  model.addConstr(gen_penalty[k] >= FSEM[k] - MSEM[k])
+  model.addConstr(g >= gen_penalty[k])
+ 
+for k in US_SEM:
+  # Minimize the Citizenship Penalty
+  model.addConstr(citizenship_penalty[k] >= MSEM[k] - FSEM[k])
+  model.addConstr(citizenship_penalty[k] >= FSEM[k] - MSEM[k])
+  model.addConstr(c >= citizenship_penalty[k])
+  
