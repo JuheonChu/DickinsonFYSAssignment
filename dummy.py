4014857151685c37deac1733691ef1581a298185
Juheon Chu <35699839+JuheonChu@users.noreply.github.com>
2023-04-10 10:52:50 -0400

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,10 +1,10 @@
-# Add constraints for Gender Penalty and Citizenship Penalty
+# Add constraints for Gender Penalty 
 for k in MSEM:
   # Minimize the Gender Penalty
   model.addConstr(gen_penalty[k] >= MSEM[k] - FSEM[k])
   model.addConstr(gen_penalty[k] >= FSEM[k] - MSEM[k])
   model.addConstr(g >= gen_penalty[k])
- 
+# Add constraints for Citizenship Penalty
 for k in US_SEM:
   # Minimize the Citizenship Penalty
   model.addConstr(citizenship_penalty[k] >= MSEM[k] - FSEM[k])
