d6b7e8a32d9e021894e0f9b8491ff4ff22fa5db0
Juheon Chu <35699839+JuheonChu@users.noreply.github.com>
2023-04-16 19:59:22 -0400

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,4 +1,5 @@
-# Minimize the imbalances in gender and number of international students.
-for k in SEMINARS:
-    model.addConstr(w_gender[k] <= GP_threshold)
-    model.addConstr(NonUS_SEM[k] <= CP_threshold)
+# Construct blended multi-objectives
+obj_functions = sum([obj_coef[1] * f_rank, obj_coef[2] * f_gender, 
+                     obj_coef[3] * f_stu_type])
+model.setObjective(obj_functions, GRB.MINIMIZE)
+model.optimize()
