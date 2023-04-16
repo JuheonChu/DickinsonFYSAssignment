eb298daf496c62d5846c32c7f22f4c0db0b663a8
Juheon Chu <35699839+JuheonChu@users.noreply.github.com>
2023-04-16 19:18:46 -0400

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,8 +1,4 @@
 # Minimize the imbalances in gender and number of international students.
 for k in SEMINARS:
-    model.addConstr(w_gender[k] >= MSEM[k] - FSEM[k])
-    model.addConstr(w_gender[k] >= FSEM[k] - MSEM[k])
-    model.addConstr(w_citizenship[k] >= US_SEM[k] - NonUS_SEM[k])
-    model.addConstr(w_citizenship[k] >= NonUS_SEM[k] - US_SEM[k])
     model.addConstr(w_gender[k] <= GP_threshold)
     model.addConstr(NonUS_SEM[k] <= CP_threshold)
