a1c686fc093cfc9c306649c7a1f681e1669b11e8
Juheon Chu <35699839+JuheonChu@users.noreply.github.com>
2023-04-16 19:16:24 -0400

    Create dummy.py

---
+++ dummy.py
@@ -0,0 +1,8 @@
+# Set the constraints for each w_gender and w_citizenship 
+for k in SEMINARS:
+    model.addConstr(w_gender[k] >= MSEM[k] - FSEM[k])
+    model.addConstr(w_gender[k] >= FSEM[k] - MSEM[k])
+    model.addConstr(w_citizenship[k] >= US_SEM[k] - NonUS_SEM[k])
+    model.addConstr(w_citizenship[k] >= NonUS_SEM[k] - US_SEM[k])
+    model.addConstr(w_gender[k] <= GP_threshold)
+    model.addConstr(NonUS_SEM[k] <= CP_threshold)
