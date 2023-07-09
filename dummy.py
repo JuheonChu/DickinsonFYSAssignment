287e72dd6d4042e41f8685518e68dee5e8b4bf71
Juheon Chu <35699839+JuheonChu@users.noreply.github.com>
2023-07-08 19:35:23 -0700

    Delete dummy.py

--- dummy.py
+++
@@ -1,5 +0,0 @@
-# Construct blended multi-objectives
-obj_functions = sum([obj_coef[1] * f_rank, obj_coef[2] * f_gender, 
-                     obj_coef[3] * f_stu_type])
-model.setObjective(obj_functions, GRB.MINIMIZE)
-model.optimize()
