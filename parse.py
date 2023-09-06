3eb65d6bf901835a67450cc0d2c9e6dcd12df683
Juheon Chu <35699839+JuheonChu@users.noreply.github.com>
2023-09-06 02:39:47 -0400

    Update parse.py

--- parse.py
+++ parse.py
@@ -10,90 +10,51 @@ Generates an excel file to be loaded into run.py
 import pandas as pd
 import xlsxwriter
 
-seminar_selection_df= pd.read_excel('2022_Seminar_Selections_reduced.xlsx', sheet_name='ag-grid')
-
-# write to sheet_name = "seminar", column: 'stu_id'
-stu_id = seminar_selection_df['Id'].tolist()
+seminar_selection_df = pd.read_excel('2023_Seminar_Selections_Initial.xlsx', sheet_name='ag-grid')
 
 
+# Function to fill in missing Seminar Ids
+def preprocess_data(group):
+    for i, row in group.iterrows():
+        if pd.isna(row['Seminar Id']):  # Check for NaN instead of zero
+            current_order = row['Selection Order']
+            target_order = {
+                1: 2,
+                2: 1,
+                3: 2,
+                4: 3,
+                5: 4,
+                6: 5
+            }.get(current_order, None)
+            
+            if target_order is not None:
+                target_seminar_id = group.loc[group['Selection Order'] == target_order, 'Seminar Id'].values[0]
+                group.at[i, 'Seminar Id'] = target_seminar_id
+                
+    return group
+
+# Apply the function to each student group
+seminar_selection_df = seminar_selection_df.groupby('Student ID').apply(preprocess_data)
+
+
+stu_id = seminar_selection_df['Student ID'].tolist()
 seminar_id = seminar_selection_df['Seminar Id'].tolist()
-
-# write to sheet_name = "seminar", column: 'rank'
+seminar_id = [int(x) if pd.notna(x) else 0 for x in seminar_id]
 rank_order = seminar_selection_df['Selection Order'].tolist()
-
-# 'M' or 'F'
 gender = seminar_selection_df['Gender'].tolist()
-
-# 'US': 1, NonUS: 0
 citizen = seminar_selection_df['Citizen Code'].tolist()
 
+# Convert M to 1 and F to 0
+gender = [1 if g == 'M' else 0 for g in gender]
+# Convert 'US' to 1 and others to 0
+citizen = [1 if c == 'US' else 0 for c in citizen]
 
-# wriet to sheet_name = "seminar", column: 'seminar'
 seminar_id_write = [None] * len(seminar_id)
-
-# Parse the Seminar Id by getting the last 2 digits
 for i in range(len(seminar_id)):
     seminar_id_write[i] = int(str(seminar_id[i])[-2:])
 
 
-# Generate a distinct student set
-# write to sheet_name = "citizenship", column: 'student_id'
-#          sheet_name = "gender", column: 'stu_id'
-
-num_students = int(len(stu_id)/6)
-
-STUDENTS = [None] * num_students
-
-for i in range(0, len(stu_id), 6):
-    idx = int(i/6)
-    STUDENTS[idx] = stu_id[i]
-    
-
-GENDER = [None] * num_students
-
-for i in range(0, len(gender), 6):
-    idx = int(i/6)
-    
-    if gender[i] == 'M':
-        GENDER[idx] = 1
-    elif gender[i] == 'F':
-        GENDER[idx] = 0
-    else:
-        # In case of Non-binary identification of gender,
-        # I meant to say 0 as "non-male"
-        GENDER[idx] = 0
-
-# shet_name = "citizenship"
-US_CITIZEN = [None] * num_students
-
-for i in range(0, len(citizen), 6):
-    idx = int(i/6)
-    
-    if citizen[i] == "US":
-        US_CITIZEN[idx] = 1
-    else:
-        # Any non-US students have '0'
-        US_CITIZEN[idx] = 0
-
-# sheet_name = "obj_coef" (column == var name)
-obj_coef_key = [1,2,3]
-obj_coef = [1,4,1]
-
-# sheet_name = "rank_weights"
-rank_index = [1,2,3,4,5,6]
-rank_weights = [-70, -60, -30, 1, 100, 300]
-
-
-set_seminar = set(seminar_id_write) 
-
-# DISTINCT SEMINAR list
-SEMINAR = (list(set_seminar))
- 
-
-# Write an Excel file 
-
-workbook = xlsxwriter.Workbook('write_dict.xlsx')
-
+workbook = xlsxwriter.Workbook('parsed.xlsx')
 worksheet_citizenship = workbook.add_worksheet('citizenship')
 worksheet_gender = workbook.add_worksheet('gender')
 worksheet_obj_coef = workbook.add_worksheet('obj_coef')
@@ -101,23 +62,22 @@ worksheet_rank_weights = workbook.add_worksheet('rank_weights')
 worksheet_seminar = workbook.add_worksheet('seminar')
 worksheet_course_num = workbook.add_worksheet('course_num')
 
-
-citizenship_dict = {'student_id': STUDENTS,
-           'citizen': US_CITIZEN}
+citizenship_dict = {'student_id': stu_id,
+           'citizen': citizen}
 
 gender_dict={
-    'stu_id':STUDENTS,
-    'gender': GENDER         
+    'stu_id':stu_id,
+    'gender': gender         
     }
 
 obj_coef_dict = {
-    'obj_coef_key':obj_coef_key,
-    'obj_coef': obj_coef
+    'obj_coef_key':[1,2,3],
+    'obj_coef': [1,4,1]
     }
 
 rank_weights_dict = {
-    'rank_index': rank_index,
-    'rank_weights': rank_weights
+    'rank_index': [1,2,3,4,5,6],
+    'rank_weights': [-70, -60, -30, 1, 100, 300]
     }
 
 seminar_dict = {
@@ -127,52 +87,44 @@ seminar_dict = {
     }
 
 course_num_dict={
-    'seminar_no': SEMINAR
+    'seminar_no': list(set(seminar_id_write))
     }
 
 col_num = 0
-
 for key, value in citizenship_dict.items():
     worksheet_citizenship.write(0, col_num, key)
     worksheet_citizenship.write_column(1, col_num, value)
     col_num += 1
 
 col_num = 0
-
 for key, value in gender_dict.items():
     worksheet_gender.write(0, col_num, key)
     worksheet_gender.write_column(1, col_num, value)
     col_num += 1
 
 col_num = 0
-
 for key, value in obj_coef_dict.items():
     worksheet_obj_coef.write(0, col_num, key)
     worksheet_obj_coef.write_column(1, col_num, value)
     col_num += 1
 
 col_num = 0
-
 for key, value in rank_weights_dict.items():
     worksheet_rank_weights.write(0, col_num, key)
     worksheet_rank_weights.write_column(1, col_num, value)
     col_num += 1
 
 col_num = 0
-
 for key, value in seminar_dict.items():
     worksheet_seminar.write(0, col_num, key)
     worksheet_seminar.write_column(1, col_num, value)
     col_num += 1
 
 col_num = 0
-
 for key, value in course_num_dict.items():
     worksheet_course_num.write(0, col_num, key)
     worksheet_course_num.write_column(1, col_num, value)
     col_num += 1    
     
-workbook.close()    
-
-    
+workbook.close()
 
